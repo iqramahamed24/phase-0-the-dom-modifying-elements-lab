a0104546bd6514a8fd3d96441d00cdeae4d99d4f
iqramahamed <iqra.mahamed64@gmail.com>
2024-04-16 18:09:46 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'IQRA is the champion';
+
+document.body.appendChild(newHeader);
